c20e6d821e4ac3ccc71394a3038986ec8ea26503
aadame3311 <antonio.adame01@utrgv.edu>
2018-11-02 16:18:01 -0500

    added connection to the database

--- public/test.php
+++ public/test.php
@@ -1,5 +1,17 @@
 <?php
-require('vendor/autoload.php');
 
-print("hello world");
+    # DATABASE VALUES.
+    $DB_HOST = 'us-cdbr-iron-east-01.cleardb.net';
+    $DB_USR = 'badb5626072670';
+    $DB_PWD = '978e7637';
+    $DB_PORT = 3306;
+    $conn = new mysqli($DB_HOST, $DB_USR, $DB_PWD);
+
+
+    if ($conn->connect_error) {
+        die("Connection gailed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+
+
 ?>
