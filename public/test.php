d0f13fa41e44c68ddd24e7188e36fa5d0ca7741a
aadame3311 <antonio.adame01@utrgv.edu>
2018-11-02 16:27:36 -0500

    minor changes

--- public/test.php
+++ public/test.php
@@ -4,10 +4,9 @@
     $DB_HOST = 'us-cdbr-iron-east-01.cleardb.net';
     $DB_USR = 'badb5626072670';
     $DB_PWD = '978e7637';
-    $conn = new mysqli($DB_HOST, $DB_USR, $DB_PWD);
-
 
     # DATABASE CONNECTION.
+    $conn = new mysqli($DB_HOST, $DB_USR, $DB_PWD);
     if ($conn->connect_error) {
         die("Connection gailed: " . $conn->connect_error);
     }
