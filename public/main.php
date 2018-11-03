fdeadb1e80d2d556300b8e60ec53910073ad4c26
aadame3311 <antonio.adame01@utrgv.edu>
2018-11-02 21:32:10 -0500

    added some more php queries

--- public/main.php
+++ public/main.php
@@ -17,21 +17,40 @@ if ($conn->connect_error) {
 }
 echo "<h1>Connected successfully</h1>";
 
-# QUERYING SQL THROUGH BASIC PHP.
+/*
+--CREATE TABLE--
+$res = $conn->query("create table employee (
+    ID int not null auto_increment primary key,
+    first_name varchar(255) not null, 
+    last_name varchar(255) not null, 
+    department_id int not null, 
+
+    foreign key(department_id) references Department(ID)
+)");
+if (!$res) {
+    die("SQL ERROR: " . $conn->error);
+}
+echo "table created succesfuly.";
+
+
+--ADD VALUES TO TABLE--
+$res = $conn->query("insert into employee(first_name, last_name, department_id) values('john', 'smith', 1)");
+if (!$res) {
+    die("SQL ERROR: " . $conn->error);
+}
+echo "Insertion was succesfull.";
+*/
+
+
 # select all first names of all employees.
 $res = $conn->query("select first_name from employee");
 if (!$res) {
-    die("SQL Error" . $conn->error);
+    die("SQL Error " . $conn->error);
 }
 foreach ($res as $names) {
     foreach ($names as $n) {
         echo $n . "<br>";
     }
 }
-
-
-#QUERYING SQL THROUGH PROPEL(PHP).
-$res = EmployeeQuery::create()->findPK(11);
-echo $res->getFirstName();
 
 ?>
