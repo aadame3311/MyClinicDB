96fc9f87f189389433e40ef74ae29c3a827ee00b
aadame3311 <antonio.adame01@utrgv.edu>
2018-11-15 17:35:03 -0600

    included twig for templating.

--- public/main.php
+++ public/main.php
@@ -1,3 +1,20 @@
 <?php
 
-require "/vendor/autoload.php";
+require "../vendor/autoload.php";
+
+$settings = ['displayErrorDetails' => true];
+
+$app = new \Slim\App(['settings' => $settings]);
+
+$container = $app->getContainer();
+$container['view'] = function($container) {
+	$view = new \Slim\Views\Twig('../templates');
+	
+	$basePath = rtrim(str_ireplace('index.php', '', 
+	$container->get('request')->getUri()->getBasePath()), '/');
+
+	$view->addExtension(
+	new Slim\Views\TwigExtension($container->get('router'), $basePath));
+	
+	return $view;
+};
