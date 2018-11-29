fd6a84ad8a5059c8b5f0fac9189f1747631cb61d
aadame3311 <antonio.adame01@utrgv.edu>
2018-11-29 12:49:01 -0600

    restyled modals

--- js/source.js
+++ js/source.js
@@ -29,58 +29,66 @@ var signupModal = new tingle.modal({
 
 
 var logininfo_form = 
-    '<h1>Login</h1>' +
-    '<form class ="login-form" action="./main.php/login">'+
-        '<input id="login-username" type="text" placeholder="Username">' +
-        '<input id="login-password" type="password" placeholder="Password">' +
-    '</form>'+
-    '<p hidden class="warning" style="color: red">wrong username or password</p>';
+    '<div class="container">'+
+        '<h3 style="text-align: center;">Login</h3>' +
+        '<form class ="login-form" action="./main.php/login">'+
+            '<input id="login-username" type="text" placeholder="Username">' +
+            '<input id="login-password" type="password" placeholder="Password">' +
+        '</form>'+
+        '<p hidden class="warning" style="color: red">wrong username or password</p>'+
+    '</div>';
+    
 // set sign up form content
 var personalinfo_form = 
-    '<h1>Sign Up</h1>'+
-    '<form class ="personal-form">'+
-        '<div class="row">'+
-            '<div class="input-field col s6"><input id="firstname" type="text"><label for="firstname">First Name</label></div>' +
-            '<div class="input-field col s6"><input id="lastname" type="text"><label for="lastname">Last Name</label></div>' +
-        '</div>'+
-        '<div class="row"><div class="input-field col s12"><input id="dob" type="text"><label for="dob">Date of Birth (MM/DD/YYYY)</label></div></div>' +
-        '<div class="row"><div class="input-field col s12"><input id="address" type="text"><label for="address">Address</label></div></div>' +
-        '<div class="row"><div class="input-field col s12"><input id="phone" type="text"><label for="phone">Phone Number</label></div></div>'+
-        '<div class="row"><div class="input-field col s12"><input id="second-phone" type="text"><label for="second-phone">Phone Number</label></div></div>'+
-        '<select id="insurance-list">'+
-            '<option value="" disabled selected>Select Insurance Provider</option>'+
-            '<option value="Company 1">Insurance Company 1</option>'+
-            '<option value="Company 2">Insurance Company 2</option>'+
-            '<option value="Company 3">Insurance Company 3</option>'+
-            '<option value="N/A">None</option>'+
-        '</select>'+
-    '</form>';
+    
+    '<div class="container">'+
+        '<h3 style="text-align: center;">Sign Up</h3>'+
+        '<form class ="personal-form">'+
+            '<div class="row">'+
+                '<div class="input-field col s6"><input id="firstname" type="text"><label for="firstname">First Name</label></div>' +
+                '<div class="input-field col s6"><input id="lastname" type="text"><label for="lastname">Last Name</label></div>' +
+            '</div>'+
+            '<div class="row"><div class="input-field col s12"><input id="dob" type="text"><label for="dob">Date of Birth (MM/DD/YYYY)</label></div></div>' +
+            '<div class="row"><div class="input-field col s12"><input id="address" type="text"><label for="address">Address</label></div></div>' +
+            '<div class="row"><div class="input-field col s12"><input id="phone" type="text"><label for="phone">Phone Number</label></div></div>'+
+            '<div class="row"><div class="input-field col s12"><input id="second-phone" type="text"><label for="second-phone">Phone Number</label></div></div>'+
+            '<select id="insurance-list">'+
+                '<option value="" disabled selected>Select Insurance Provider</option>'+
+                '<option value="Company 1">Insurance Company 1</option>'+
+                '<option value="Company 2">Insurance Company 2</option>'+
+                '<option value="Company 3">Insurance Company 3</option>'+
+                '<option value="N/A">None</option>'+
+            '</select>'+
+        '</form>'+
+    '</div>';
 // set sign up form content.
 var signupinfo_form = 
-    '<h1>Sign Up</h1>'+
-    '<form class="signup-form" action="./main.php/signup">'+
-        '<div class="input-field">'+
-            '<input id="signup-email" type="email">' +
-            '<label for="signup-email">Email</label>'+
-        '</div>'+
-        '<div class="input-field">'+
-            '<input id="signup-username" type="text">' +
-            '<label for="signup-username">Username</label>'+
-        '</div>'+
-        '<div class="input-field">'+
-            '<input id="signup-password" type="password">' +
-            '<label for="signup-password">Password</label>'+
-        '</div>'+
-        '<div class="input-field">'+
-            '<input id="signup-cPassword" type="password">' +
-            '<label for="signup-cPassword">Confirm Password</label>'+
-        '</div>'+
-    '</form>';
+    '<div class="container">'+
+        '<h3 style="text-align: center;">Sign Up</h3>'+
+        '<form class="signup-form" action="./main.php/signup">'+
+            '<div class="input-field">'+
+                '<input id="signup-email" type="email">' +
+                '<label for="signup-email">Email</label>'+
+            '</div>'+
+            '<div class="input-field">'+
+                '<input id="signup-username" type="text">' +
+                '<label for="signup-username">Username</label>'+
+            '</div>'+
+            '<div class="input-field">'+
+                '<input id="signup-password" type="password">' +
+                '<label for="signup-password">Password</label>'+
+            '</div>'+
+            '<div class="input-field">'+
+                '<input id="signup-cPassword" type="password">' +
+                '<label for="signup-cPassword">Confirm Password</label>'+
+            '</div>'+
+        '</form>'+
+    '</div>';
 
 
 // PERSONAL INFORMATION THAT IS SENT TO SIGNUP //
 personalinfoModal.setContent(personalinfo_form);
-personalinfoModal.addFooterBtn('Exit', 'btn waves-effect waves-light tingle-btn--pull-left', function() {
+personalinfoModal.addFooterBtn('Exit', 'btn btn-danger waves-effect waves-light tingle-btn--pull-left', function() {
     personalinfoModal.close();
 });
 personalinfoModal.addFooterBtn('Next', 'btn waves-effect waves-light tingle-btn--pull-right', function() {
@@ -101,7 +109,7 @@ personalinfoModal.addFooterBtn('Next', 'btn waves-effect waves-light tingle-btn-
 });
 // SIGNUP FORM //
 signupModal.setContent(signupinfo_form);
-signupModal.addFooterBtn('Back', 'btn waves-effect waves-light tingle-btn--pull-left', function() {
+signupModal.addFooterBtn('Back', 'btn btn-danger waves-effect waves-light tingle-btn--pull-left', function() {
     signupModal.close();
     personalinfoModal.open();
 });
